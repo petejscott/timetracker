514962c780ec7d6539a514046f12fd69e28dc7c6
Pete Scott <pete.j.scott@gmail.com>
2015-10-26 21:13:00 -0400

    Some test cleanup too

--- test/appDataTests.js
+++ test/appDataTests.js
@@ -1,14 +1,14 @@
 QUnit.test( "appData.setConfig and getConfig return same config", function( assert ) {
 	var expectedConfig = { 'foo' : 'bar' };
-	appDataInstance = new AppData();
-    appDataInstance.setConfig(expectedConfig);
-    assert.ok( appDataInstance.getConfig() === expectedConfig );
+	var appData = new AppData();
+    appData.setConfig(expectedConfig);
+    assert.ok( appData.getConfig() === expectedConfig );
 });
 
 QUnit.test( "appData.addGroup adds group to collection", function( assert ) {
     var group = tt.groupFactory.createNewGroup();
-    appDataInstance = new AppData();
-    assert.ok( appDataInstance.groups.length === 0 );
-    appDataInstance.addGroup(group);
-    assert.ok( appDataInstance.groups.length === 1 );
+    var appData = new AppData();
+    assert.ok( appData.groups.length === 0 );
+    appData.addGroup(group);
+    assert.ok( appData.groups.length === 1 );
 });
