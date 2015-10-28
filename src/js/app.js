11deeb893e6bef3d9c252a17e89a50fe28858ac1
Pete Scott <pete.j.scott@gmail.com>
2015-10-27 22:33:36 -0400

    rework app syncing
    Should work on new objects now

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,35 @@ tt.app = (function(logger, groupFactory, taskFactory, viewFactory, eventService)
 	var appData = new AppData();
     Object.seal(appData);
     appData.setConfig(tt.config);
-	
+    registerAppDataSync(appData);
+
+    function registerAppDataSync(appData) {
+        appData.subscribe('group-added', requestHighPrioritySync);
+        appData.subscribe('group-removed', requestHighPrioritySync);
+
+        appData.subscribe('group-added', function(e) { registerGroupSync(e.detail.group); });
+        for (var i = 0, len = appData.groups.length; i < len; i++) {
+            registerGroupSync(appData.groups[i]);
+        }
+    }
+
+    function registerGroupSync(group) {
+        group.subscribe('group-title-modified', requestHighPrioritySync);
+        group.subscribe('total-modified', requestLowPrioritySync);
+        group.subscribe('task-added', requestHighPrioritySync);
+        group.subscribe('task-removed', requestHighPrioritySync);
+
+        group.subscribe('task-added', function(e) { registerTaskSync(e.detail.task); });
+        for (var i = 0, len = group.tasks.length; i < len; i++) {
+           registerTaskSync(group.tasks[i]);
+        }
+    }
+
+    function registerTaskSync(task) {
+        task.subscribe('task-title-modified', requestHighPrioritySync);
+        task.subscribe('task-state-toggled', requestHighPrioritySync);
+    }
+
 	function groupsRetrievedEventHandler(e) {
 		
 		var storedGroups = e.detail;
@@ -16,31 +44,12 @@ tt.app = (function(logger, groupFactory, taskFactory, viewFactory, eventService)
 			}
 		}
 		createViews();
-        scheduleSync();
 	}
 	
 	function createViews() {
 		viewFactory.makeNavigationView(appData, groupFactory, taskFactory);
 		viewFactory.makeSyncStatusView(requestHighPrioritySync);
 	}
-
-    function scheduleSync() {
-        appData.subscribe('group-added', requestHighPrioritySync);
-        appData.subscribe('group-removed', requestHighPrioritySync);
-
-        for (var i = 0, gLen = appData.groups.length; i < gLen; i++) {
-            var group = appData.groups[i];
-            group.subscribe('group-title-modified', requestHighPrioritySync);
-            group.subscribe('total-modified', requestLowPrioritySync);
-            group.subscribe('task-added', requestHighPrioritySync);
-            group.subscribe('task-removed', requestHighPrioritySync);
-            for (var j = 0, tLen = group.tasks.length; j < tLen; j++) {
-                var task = group.tasks[j];
-                task.subscribe('task-title-modified', requestHighPrioritySync);
-                task.subscribe('task-state-toggled', requestHighPrioritySync);
-            }
-        }
-    }
 
     function requestHighPrioritySync() {
         requestSync('high');
